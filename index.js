38f6eab8c428f1042b86367ea945ddda3fc80ec2
Richard Goater <richard.goater27@gmail.com>
2016-04-05 14:24:08 +0100

    Converted to js config file

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: require.resolve('eslint-config-airbnb'),
+  parser: require.resolve('babel-eslint'),
+  rules: {
+    'spaced-comment': 2,         // http://eslint.org/docs/rules/spaced-comment
+    'object-curly-spacing': [ 2, 'always' ], // http://eslint.org/docs/rules/object-curly-spacing
+    'array-bracket-spacing': [ 2, 'always' ], // http://eslint.org/docs/rules/array-bracket-spacing
+    'max-len': [ 1, 80, 2 ],            // http://eslint.org/docs/rules/max-len
+    'max-params': [ 1, 3 ],             // http://eslint.org/docs/rules/max-params
+    'handle-callback-err': [ 2, 'error' ], // http://eslint.org/docs/rules/handle-callback-err
+    'func-names': 0,                 // http://eslint.org/docs/rules/func-names
+    'space-before-function-paren': [ 2, { // http://eslint.org/docs/rules/space-before-function-paren
+      'anonymous': 'always',
+      'named': 'never'
+    } ],
+    'no-undef': 2,                 // http://eslint.org/docs/rules/no-undef
+    'react/no-multi-comp': 0,
+  },
+};
