79b1694776d889a2d0e213dc3220ded75fc5a58e
Richard Goater <richard.goater27@gmail.com>
2016-10-23 15:42:52 +0100

    Add eslint-plugin-import

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 module.exports = {
-  extends: require.resolve('eslint-config-airbnb'),
-  parser: require.resolve('babel-eslint'),
+  extends: [
+    'airbnb',
+    'plugin:import/errors',
+  ],
+  parser: 'babel-eslint',
   rules: {
-    'spaced-comment': 2,         // http://eslint.org/docs/rules/spaced-comment
+    'spaced-comment': 2, // http://eslint.org/docs/rules/spaced-comment
     'object-curly-spacing': [ 2, 'always' ], // http://eslint.org/docs/rules/object-curly-spacing
     'array-bracket-spacing': [ 2, 'always' ], // http://eslint.org/docs/rules/array-bracket-spacing
-    'max-len': [ 1, 80, 2 ],            // http://eslint.org/docs/rules/max-len
-    'max-params': [ 1, 3 ],             // http://eslint.org/docs/rules/max-params
+    'max-len': [ 1, 80, 2 ], // http://eslint.org/docs/rules/max-len
+    'max-params': [ 1, 3 ], // http://eslint.org/docs/rules/max-params
     'handle-callback-err': [ 2, 'error' ], // http://eslint.org/docs/rules/handle-callback-err
-    'func-names': 0,                 // http://eslint.org/docs/rules/func-names
+    'func-names': 0, // http://eslint.org/docs/rules/func-names
     'space-before-function-paren': [ 2, { // http://eslint.org/docs/rules/space-before-function-paren
       'anonymous': 'always',
       'named': 'never'
     } ],
-    'no-undef': 2,                 // http://eslint.org/docs/rules/no-undef
+    'no-undef': 2, // http://eslint.org/docs/rules/no-undef
     'react/no-multi-comp': 0,
     'react/prefer-es6-class': 0,
     'react/prefer-stateless-function': 0,
