6668e95e1bc9c906fe23ed40aad1935e94958eee
Richard Goater <richard.goater27@gmail.com>
2019-02-07 17:33:42 +0000

    Use hooks 😏

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports = {
-  extends: [
-    './airbnb/index.js',
-    'plugin:import/errors',
-  ],
+  extends: [ './airbnb/index.js', 'plugin:import/errors' ],
   parser: 'babel-eslint',
   rules: {
     'spaced-comment': 2, // http://eslint.org/docs/rules/spaced-comment
@@ -12,15 +9,20 @@ module.exports = {
     'max-params': [ 1, 3 ], // http://eslint.org/docs/rules/max-params
     'handle-callback-err': [ 2, 'error' ], // http://eslint.org/docs/rules/handle-callback-err
     'func-names': 0, // http://eslint.org/docs/rules/func-names
-    'space-before-function-paren': [ 2, { // http://eslint.org/docs/rules/space-before-function-paren
-      anonymous: 'always',
-      named: 'never',
-    } ],
+    'space-before-function-paren': [
+      2,
+      {
+        // http://eslint.org/docs/rules/space-before-function-paren
+        anonymous: 'always',
+        named: 'never',
+      },
+    ],
     'no-undef': 2, // http://eslint.org/docs/rules/no-undef
     'react/no-multi-comp': 0,
     'react/prefer-es6-class': 0,
     'react/prefer-stateless-function': 0,
     'no-whitespace-before-property': 2,
     'newline-per-chained-call': [ 2, { ignoreChainWithDepth: 4 } ],
+    'react-hooks/rules-of-hooks': 'error',
   },
 };
